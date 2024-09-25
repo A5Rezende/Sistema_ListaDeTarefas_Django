41b72bc39034bab384aa8a8620586bd8f1ebfa1f
Antonio Rezende <antoniororlandini@gmail.com>
2024-09-24 21:04:28 -0300

    first commit

---
+++ projeto/app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Tarefa, Usuario
+
+class TarefaForm(forms.ModelForm):
+    class Meta:
+        model = Tarefa
+        fields = ['titulo', 'prazo', 'responsavel', 'status']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'prazo': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'responsavel': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['nome']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+        }
---
+++ projeto/projeto/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import listar_tarefas, criar_tarefa, editar_tarefa, excluir_tarefa
+from app.usuario_views import listar_usuarios, criar_usuario, editar_usuario, excluir_usuario
+from app.api_views import tarefas_api, tarefas_detalhes_api
+
+urlpatterns = [
+    path('', listar_tarefas, name='rotaTarefas'),
+    path('nova/', criar_tarefa, name='rotaNovaTarefa'),
+    path('editar/<int:id>/', editar_tarefa, name='rotaEditarTarefa'),
+    path('excluir/<int:id>/', excluir_tarefa, name='rotaExcluirTarefa'),
+
+    path('usuarios/', listar_usuarios, name='rotaUsuarios'),
+    path('usuarios/nova/', criar_usuario, name='rotaNovoUsuario'),
+    path('usuarios/editar/<int:id>/', editar_usuario, name='rotaEditarUsuario'),
+    path('usuarios/excluir/<int:id>/', excluir_usuario, name='rotaExcluirUsuario'),
+
+    path('api/tarefas/', tarefas_api, name='apiTarefas'),
+    path('api/tarefas/<int:id>', tarefas_detalhes_api, name='apiTarefasDetalhes'),
+
+]
